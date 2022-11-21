e5d56385f387d75a08d5ffabbba1aff5175fa7ef
Mohamed Ismail <smartismail13@gmail.com>
2022-11-21 12:53:56 +0530

    'minor_changes_on_header_component'

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  open(type: string): void {
+    if (type === 'navList') {
+      // navList functionality code here
+    } else if (type === 'search') {
+      // navList functionality code here
+    }
   }
 
 }
